e270caf06120ad5d6a16345d16b19d715f5fbcea
Hughsin23 <hughsy_23@hotmail.com>
2024-03-30 13:42:50 +1100

    added shuffle.js and shuffle function

---
+++ my-app/src/utilities/shuffle.js
@@ -0,0 +1,19 @@
+const shuffle = () => {
+  const assets = [
+    { image: '/assets/maguire-cry.gif' },
+    { image: '/assets/maguire-dance.gif' },
+    { image: '/assets/maguire-hair-flip.gif' },
+    { image: '/assets/maguire-throwdown.gif' },
+    { image: '/assets/spidey-huh.png' },
+    { image: '/assets/spidey-ohno.png' },
+    { image: '/assets/spidey-pic.png' },
+    { image: '/assets/spidey-ohno.png' },
+  ]
+
+  return [...assets, ...assets]
+    .sort(() => Math.random() = 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+
+}
+
+export default shuffle
