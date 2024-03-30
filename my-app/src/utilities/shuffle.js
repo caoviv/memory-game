dcf7ed6625958cd826ee13794f8c1f7b941d5071
Vivian C <viviancao58@gmail.com>
2024-03-30 14:16:15 +1100

    fix image name typo

--- my-app/src/utilities/shuffle.js
+++ my-app/src/utilities/shuffle.js
@@ -1,19 +1,18 @@
 const shuffle = () => {
   const assets = [
-    { image: '/assets/maguire-cry.gif' },
-    { image: '/assets/maguire-dance.gif' },
-    { image: '/assets/maguire-hair-flip.gif' },
-    { image: '/assets/maguire-throwdown.gif' },
-    { image: '/assets/spidey-huh.png' },
-    { image: '/assets/spidey-ohno.png' },
-    { image: '/assets/spidey-pic.png' },
-    { image: '/assets/spidey-ohno.png' },
-  ]
+    { image: "/assets/maguire-cry.gif" },
+    { image: "/assets/maguire-dance.gif" },
+    { image: "/assets/maguire-hair-flip.gif" },
+    { image: "/assets/maguire-throwdown.gif" },
+    { image: "/assets/spidey-huh.png" },
+    { image: "/assets/spidey-ohno.png" },
+    { image: "/assets/spidey-pic.png" },
+    { image: "/assets/spidey-ok.png" },
+  ];
 
   return [...assets, ...assets]
     .sort(() => Math.random() - 0.5)
-    .map((card) => ({ ...card, id: Math.random() }))
+    .map((card) => ({ ...card, id: Math.random() }));
+};
 
-}
-
-export default shuffle
+export default shuffle;
