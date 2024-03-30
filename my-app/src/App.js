615dd7411e717ad94bc7f93830516835c65e719c
Hughsin23 <hughsy_23@hotmail.com>
2024-03-30 13:45:52 +1100

    added shuffle to app, as well as state useage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 import Card from "./components/Card";
+import shuffle from "./utilities/shuffle";
+
 function App() {
+  const [cards, setCards] = useState(shuffle)
   return (
     <div className="App">
-      <Card key={id} image={image} selected={false} onClick={() => {}} />
+      <Card key={id} image={image} selected={false} onClick={() => { }} />
     </div>
   );
 }
