46c8cd546d440b526204a8e782bc89da044694e9
Vivian C <viviancao58@gmail.com>
2024-03-30 13:41:42 +1100

    add img to card components

--- my-app/src/components/Card.jsx
+++ my-app/src/components/Card.jsx
@@ -1,5 +1,12 @@
-const Card = () => {
-  return <div></div>;
+const Card = ({ image, selected, onClick }) => {
+  return (
+    <div className="card">
+      <div className={selected && "selected"}>
+        <img alt="" src={image} className="card-face" />
+        <img alt="" className="card-back" src={"/asset/spidey-point.png"} />
+      </div>
+    </div>
+  );
 };
 
 export default Card;
